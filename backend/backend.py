20b286ee84bc840d690d6af1ba7570c5cde024e0
Pedro from 42 <pearredo@c2r4s1.42malaga.com>
2026-04-22 19:15:17 +0200

    empty page with flask 10pts

--- backend/backend.py
+++ backend/backend.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def blank():
 	return "", 200
+
+
+@app.route("/healthz")
+def healthz():
+	return jsonify({"status": "ok"}), 200
+
 
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=3000)
